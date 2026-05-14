865171060ad32ec885429d70c7e45e70180ea3fd
dev <dev@wormworld.local>
2026-05-14 20:34:17 +0200

    Keep flag worm fixed size while preview box stays wide

--- app/(shop)/index.tsx
+++ app/(shop)/index.tsx
@@ -942,13 +942,10 @@ const styles = StyleSheet.create({
     position: "relative",
   },
   previewCanvasImage: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    width: "100%",
-    height: "100%",
+    alignSelf: "center",
+    width: 410,
+    height: 150,
+    maxWidth: "100%",
   },
   previewTube: {
     position: "absolute",
